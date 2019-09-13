6fa389b43ca82518ec8e90ac63a071bd3e133f1b
viparadkar@gmail.com <viparadkar@gmail.com>
2019-09-13 10:09:12 +0530

    first commit with only library and console app

---
+++ TddCalApp.Library/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddCalApp.Library
+{
+    public class Calculator
+    {
+        public int Add(int num1,int num2)
+        {
+            return 0; 
+        }
+    }
+}
