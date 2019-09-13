69fc1b0662f24aa6c9a6f1e8baa5be9280a5ee08
viparadkar@gmail.com <viparadkar@gmail.com>
2019-09-13 10:39:24 +0530

    commit with one positive and negative number

--- TddCalApp.Test/UnitTest1.cs
+++ TddCalApp.Test/UnitTest1.cs
@@ -16,7 +16,16 @@ namespace TddCalApp.Test
             int result;
             result = testCalculator.Add(num1, num2);
             Assert.AreEqual(5, result, "testing two integers");
-
+        }
+        [TestMethod]
+        public void ShouldReturnZeroonPassingPairOfPositiveAndNegativeNumber()
+        {
+            Calculator testCalculator = new Calculator();
+            int num1 = -3;
+            int num2 = +3;
+            int result;
+            result = testCalculator.Add(num1, num2);
+            Assert.AreEqual(0, result, "testing of pair of one positive and negative");
         }
     }
 }
